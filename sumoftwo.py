555e4b8eb12532e91eface635c0b13b61c4c9d92
knixk <65497298+knixk@users.noreply.github.com>
2021-03-26 18:27:42 +0530

    Update sumoftwo.py

--- sumoftwo.py
+++ sumoftwo.py
@@ -1,7 +1,7 @@
-val1 = int(input())
-val2 = int(input())
+val1 = int(input())                     #Declared two variables.
+val2 = int(input())                     
 
 
-sum = int(float(val1) + float(val2))
+sum = int(float(val1) + float(val2))    #added both the vals and created sum variable.
 
-print(sum)
+print(sum)                              #printed the sum
