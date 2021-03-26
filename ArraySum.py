9225a51e129f80d384a05b4153400583a5eeaaaf
knixk <65497298+knixk@users.noreply.github.com>
2021-03-26 18:29:28 +0530

    Update ArraySum.py

--- ArraySum.py
+++ ArraySum.py
@@ -10,10 +10,10 @@ def simpleArraySum(ar):
     #
     # Write your code here.
     #
-    sum = 0
-    for i in ar:
-        sum += i
-    return sum
+    sum = 0                                               #initialized the sum variable
+    for i in ar:                                          #looping through the array ar
+        sum += i                                          #we increment the current position of i
+    return sum                                            #then we return the sum and hacker rank outputs the result
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
