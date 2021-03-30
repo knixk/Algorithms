838db06fa618c08c6e4ab156be518309955a11c3
knixk <65497298+knixk@users.noreply.github.com>
2021-03-30 15:54:09 +0530

    Update nthstairs.py

--- nthstairs.py
+++ nthstairs.py
@@ -1,15 +1,17 @@
 # Solution 1
-N = int(10)
+N = int(10)                                                 
 
 def steps(N):
     lst=[0,1,2]                           
     for i in range (3,N+1):
-       sum=lst[i-1]+lst[i-2]
+       sum=lst[i-1]+lst[i-2]                     #You would be hearing terms like fibonacci numbers
        lst.append(sum)
     return lst[N]
 
 print(steps(N))
 
+
+#---------------------------------------------------------------------------------------------------
 #Solution 2 
 
 
@@ -27,8 +29,8 @@ def climb(n):
 
     for i in range(3, n+1):
         tmp = y
-        y += x
-        x = tmp
+        y += x                       #using the sliding window approach                                   
+        x = tmp                      #we set y as the next number and x as the previous number
     return y
 
 print(climb(n))
