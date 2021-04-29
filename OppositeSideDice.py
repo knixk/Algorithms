f90a914678e5291d45074d7b55cf7d0be7f48004
Kanishk Shrivastava <65497298+knixk@users.noreply.github.com>
2021-04-29 14:48:23 +0530

    Create OppositeSideDice.py

---
+++ OppositeSideDice.py
@@ -0,0 +1,14 @@
+
+def oppositeFaceOfDice(self, N):
+    if N == 1:
+        return "6"
+    elif N == 2:
+        return "5"
+    elif N == 3:
+        return "4"
+    elif N == 4:
+        return "3"
+    elif N == 5:
+        return "2"
+    else:
+        return "1"
